7f51f1fea5af084716f86cd0dc4bb81d34fcf36d
HeitorSamuel <166461014+HeitorSamuel@users.noreply.github.com>
2026-05-12 11:24:01 -0300

    imc parte 2

--- script/script.js
+++ script/script.js
@@ -9,19 +9,22 @@ function calcular(){
         if (altura <= 0 || peso <= 0){
             window.alert('Digite caracteres válidos nos campos correspondentes!')
         }else{
+            var res = document.getElementById('res')
+            document.getElementById('tela-resultado').classList.remove('ocultar-res')
+            
             var imc = peso/(altura**2)
             if (imc < 18.5){
-                res.innerHTML = `<p>O seu IMC é ${imc}, você está Abaixo do Peso!</p>`
-            }else if (18.5 <= imc <= 24.9){
-                res.innerHTML = `<p>O seu IMC é ${imc}, o seu Peso está Normal!</p>`
-            }else if (25.0 <= imc <= 29.9){
-                res.innerHTML = `<p>O seu IMC é ${imc}, você está com Sobrepeso!</p>`
-            }else if (30.0 <= imc <= 34.9){
-                res.innerHTML = `<p>O seu IMC é ${imc}, você está com Obesidade de Grau I!</p>`
-            }else if (35.0 <= imc <= 39.9){
-                res.innerHTML = `<p>O seu IMC é ${imc}, você com Obesidade de Grau II!</p>`
+                res.innerHTML = `<p>O seu IMC é ${imc.toFixed(2)}, você está Abaixo do Peso!</p>`
+            }else if (imc >= 18.5 && imc <= 24.9){
+                res.innerHTML = `<p>O seu IMC é ${imc.toFixed(2)}, o seu Peso está Normal!</p>`
+            }else if (imc >= 25.0 && imc <= 29.9){
+                res.innerHTML = `<p>O seu IMC é ${imc.toFixed(2)}, você está com Sobrepeso!</p>`
+            }else if (imc >= 30.0 && imc <= 34.9){
+                res.innerHTML = `<p>O seu IMC é ${imc.toFixed(2)}, você está com Obesidade de Grau I!</p>`
+            }else if (imc >= 35.0 && imc <= 39.9){
+                res.innerHTML = `<p>O seu IMC é ${imc.toFixed(2)}, você com Obesidade de Grau II!</p>`
             }else if (imc >= 40.0){
-                res.innerHTML = `<p>O seu IMC é ${imc}, você com Obesidade de Grau III!</p>`
+                res.innerHTML = `<p>O seu IMC é ${imc.toFixed(2)}, você com Obesidade de Grau III!</p>`
             }
             
         }
@@ -29,7 +32,13 @@ function calcular(){
 }
 
 function limpar(){
-            altura.value = ''
-            peso.value = ''
-            res.innerHTML = ''
+    var txtalt = document.getElementById('txtalt')
+    var txtpeso = document.getElementById('txtpeso')
+    var res = document.getElementById('res')
+    var telaResult = document.getElementById('tela-resultado')
+    txtalt.value = ''
+    txtpeso.value = ''
+    res.innerHTML = ''
+    telaResult.classList.add('ocultar-res')
+    txtalt.focus()
 }
